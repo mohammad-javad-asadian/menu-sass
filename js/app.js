936181a8ce1f9249edf710d65189a94c5cdf37d2
mohammad-javad-asadian <asadian.mjak20@gmail.com>
2026-06-29 10:47:30 +0330

    dainamik code js of subMenu

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 "use strict"
 const $ = document
-const menu = $.querySelectorAll('.nav_list_items')[1]
-const subMenu = $.querySelectorAll('.nav_list_items_subList_off')[0]
+const menu = $.querySelectorAll('.nav_list_items')
+const subMenu = $.querySelectorAll('.nav_list_items_subList_off')
 let bolSubMenu = false
-menu.addEventListener('click', () => {
 
-    if (bolSubMenu) {
-        subMenu.className = "nav_list_items_subList_off"
-        bolSubMenu = false
-    } else {
-        subMenu.className = "nav_list_items_subList_on"
-        bolSubMenu = true
-        $.body.addEventListener('click', () => {
-            subMenu.className = "nav_list_items_subList_off"
-            bolSubMenu = false
-        }, { capture: true })
-    }
+let keyUl = ['home', 'shop', 'category', 'about']
+menu.forEach(a => {
+    a.addEventListener('click', () => {
+        if (keyUl.includes(a.dataset.key) && !bolSubMenu) {
+            subMenu.forEach(event => {
+                if (event.dataset.key == a.dataset.key) {
+                    event.className = "nav_list_items_subList_on"
+                    bolSubMenu = true
+                    $.body.addEventListener('click', () => {
+                        event.className = "nav_list_items_subList_off"
+                        bolSubMenu = false
+                    }, { capture: true })
+                }
+            })
+        }
+    })
 })
